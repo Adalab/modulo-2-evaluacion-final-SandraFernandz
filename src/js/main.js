f3dea8c321c7f56f11a199e4a963ac034475b7f5
Sandra Fernandz <sandraenmunich@gmail.com>
2021-09-09 17:16:18 +0200

    adding first html and css styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const button = document.querySelector(".js_button");
+
+//variable local que almacena el resultado de la búsqueda de las series introducida en el input de texto
+let series = [];
+
+//función manejadora del evento click de button.addEventListener para que al hacer click en el botón, la aplicación se conecta a la api de TVMaze
+function handleConnectTv(ev) {
+  fetch("https://api.tvmaze.com/search/shows?q=girls")
+    .then((response) => response.json())
+    .then((data) => {});
+  series = data.series;
+  //función para que la búsqueda del input resulte en un listado de series
+  console.log(response);
+}
+
+button.addEventListener("click", handleConnectTv);
+
+console.log("holis");
